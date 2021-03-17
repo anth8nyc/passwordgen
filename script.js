f2b90f34289048b65b3870f68d5466d6951d5c3a
Anthony Castillo <anthonycastillo821@gmail.com>
2021-03-17 15:53:14 -0500

    added character arrays to code

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Arrays of characters
+
+let lletters = "abcdefghijklmnopqrstuvwxyz".split("") 
+let uletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+let numberc = "1234567890".split("")
+let specialc = " !U+0022#$%&U+0027()*+,-./:;<=>?@[\]^_`{|}~".split("")
+
+console.log(lletters)
+console.log(uletters)
+console.log(numberc)
+console.log(specialc)
+

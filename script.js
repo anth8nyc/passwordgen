e8100b1f15441155265b3ab9e27254d333ca03df
Anthony Castillo <anthonycastillo821@gmail.com>
2021-03-18 18:57:20 -0500

    added no value or space bar input to error message trigger criteria

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// Arrays of characters
 
 let lletters = "abcdefghijklmnopqrstuvwxyz".split(""); 
 let uletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
@@ -16,22 +12,18 @@ function writePassword() {
   
   let charcount = prompt("Specify password character count with number between 8 - 128: ");
   
-  if (isNaN(charcount)) {
+  if ((isNaN(charcount))||(charcount === "")||(charcount.includes(" "))){
     alert("Please input a number value specifying a password length between 8 - 128 characters and try again.")
     return      
-  } else {
-    
-  }
+  } else { }
   
   let charcountn = parseInt(charcount, 10);
   
   if ((charcountn > 128)||(charcountn < 8)) {
     alert("Password character length must be between 8 - 128 characters-- please try again.")
     return
-  } else{
-  }
+  } else { }
   
-
   let loinclude = confirm("Include lowercase character?")
   let upinclude = confirm("Include uppercase characters?");
   let numinclude = confirm("Include number characters?");
@@ -41,49 +33,40 @@ function writePassword() {
 
     alert("At least one character type must be selected. Please try again for different character type.")
     return
-  } else { 
-
-  }
+  } else { }
   
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
-  
+  console.log(password)
   
   function generatePassword(){
     
-    
+    let options = buildoptions();
+    var output = "";
+    var asize = options.length;
+    for (var i = 0; i < charcountn; ++i) {
+      output = output + options[(Math.floor(Math.random() * asize))];
+    } return output;   
   
-    
-    
-
     function buildoptions(){
       
-
       let options = [];
 
       if (loinclude === true) {
-        
         options = options.concat(lletters)
       }
-      
       if (upinclude === true) {
-        
         options = options.concat(uletters)
       }
-      
       if (numinclude === true) {
-        
         options = options.concat(numberc)
       }
-      
       if (spcinclude === true) {
-        
         options = options.concat(specialc)
       }
 
-      
       console.log(lletters)
       console.log(uletters)
       console.log(numberc)
@@ -92,31 +75,10 @@ function writePassword() {
       console.log(options)
       return options
 
-    }       
-      
-    let options = buildoptions();
-    var output = "";
-    var asize = options.length;
-    for (var i = 0; i < charcountn; ++i) {
-      output = output + options[(Math.floor(Math.random() * asize))];
-    } return output;    
+    }        
     
   }  
 
 }
 
 generateBtn.addEventListener("click", writePassword);
-
-// if ((charcountn < 129) && (charcountn > 7)) {
-  
-//  function buildoptions()
-  
-  
-// }else {
-//   alert("Character count must be between 8 - 128 characters!");
-//   function generatePassword();
-// }
-
-// Add event listener to generate button
-
-

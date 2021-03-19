e0aeaf9e3470019e0f691e6b4ffcbc77dd6a0989
Anthony Castillo <anthonycastillo821@gmail.com>
2021-03-18 19:32:41 -0500

    cleaning code

--- script.js
+++ script.js
@@ -1,84 +1,63 @@
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 let lletters = "abcdefghijklmnopqrstuvwxyz".split(""); 
 let uletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 let numberc = "1234567890".split("");
-let specialc1 = ["'", '"', "\\"];
-let specialc2 = " !#$%&()*+,-./:;<=>?@[]^_`{|}~".split("");
+  let specialc1 = ["'", '"', "\\"];
+  let specialc2 = " !#$%&()*+,-./:;<=>?@[]^_`{|}~".split("");
 let specialc = specialc1.concat(specialc2);
-let allc = lletters.concat(uletters, numberc, specialc)
 
 function writePassword() {
   
   let charcount = prompt("Specify password character count with number between 8 - 128: ");
   
   if ((isNaN(charcount))||(charcount === "")||(charcount.includes(" "))){
-    alert("Please input a number value specifying a password length between 8 - 128 characters and try again.")
+    alert("Please input a number value specifying a password length between 8 - 128 characters and try again.");
     return      
   } else { }
   
-  let charcountn = parseInt(charcount, 10);
+  let charcountn = parseInt(charcount);
   
-  if ((charcountn > 128)||(charcountn < 8)) {
-    alert("Password character length must be between 8 - 128 characters-- please try again.")
+  if ((charcountn > 128)||(charcountn < 8)){
+    alert("Password character length must be between 8 - 128 characters-- please try again.");
     return
   } else { }
   
-  let loinclude = confirm("Include lowercase character?")
+  let loinclude = confirm("Include lowercase character?");
   let upinclude = confirm("Include uppercase characters?");
   let numinclude = confirm("Include number characters?");
   let spcinclude = confirm("Include special characters?");
 
   if ((loinclude === false)&&(upinclude === false)&&(numinclude === false)&&(spcinclude === false)){
-
-    alert("At least one character type must be selected. Please try again for different character type.")
+    alert("At least one character type must be selected. Please try again for different character type.");
     return
   } else { }
   
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
   
   passwordText.value = password;
-  console.log(password)
   
   function generatePassword(){
     
     let options = buildoptions();
-    var output = "";
-    var asize = options.length;
-    for (var i = 0; i < charcountn; ++i) {
+    let output = "";
+    let asize = options.length;
+
+    for (let i = 0; i < charcountn; ++i) {
       output = output + options[(Math.floor(Math.random() * asize))];
     } return output;   
   
     function buildoptions(){
       
       let options = [];
-
-      if (loinclude === true) {
-        options = options.concat(lletters)
-      }
-      if (upinclude === true) {
-        options = options.concat(uletters)
-      }
-      if (numinclude === true) {
-        options = options.concat(numberc)
-      }
-      if (spcinclude === true) {
-        options = options.concat(specialc)
-      }
-
-      console.log(lletters)
-      console.log(uletters)
-      console.log(numberc)
-      console.log(specialc)
-      console.log(allc)
-      console.log(options)
+      if (loinclude === true) {options = options.concat(lletters);}
+      if (upinclude === true) {options = options.concat(uletters);}
+      if (numinclude === true) {options = options.concat(numberc);}
+      if (spcinclude === true) {options = options.concat(specialc);}
       return options
-
     }        
-    
   }  
-
 }
 
 generateBtn.addEventListener("click", writePassword);
